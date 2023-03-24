e7fd0869db4f2edab5aa903433895a3433d79bbb
Mark <36869660+SchubyDong@users.noreply.github.com>
2023-03-24 14:32:39 -0400

    0322 - Switch PPV to Mat

---
+++ Binaries/Android/Uninstall_Grid_Glow_Discharge-Android-Shipping-armv7.bat
@@ -0,0 +1,19 @@
+setlocal
+if NOT "%UE_SDKS_ROOT%"=="" (call %UE_SDKS_ROOT%\HostWin64\Android\SetupEnvironmentVars.bat)
+set ANDROIDHOME=%ANDROID_HOME%
+if "%ANDROIDHOME%"=="" set ANDROIDHOME=C:/Users/Heav107pc15/AppData/Local/Android/Sdk
+set ADB=%ANDROIDHOME%\platform-tools\adb.exe
+set DEVICE=
+if not "%1"=="" set DEVICE=-s %1
+for /f "delims=" %%A in ('%ADB% %DEVICE% shell "echo $EXTERNAL_STORAGE"') do @set STORAGE=%%A
+@echo.
+@echo Uninstalling existing application. Failures here can almost always be ignored.
+%ADB% %DEVICE% uninstall com.CryoVR.Grid_Glow_Discharge
+@echo.
+echo Removing old data. Failures here are usually fine - indicating the files were not on the device.
+%ADB% %DEVICE% shell rm -r %STORAGE%/UE4Game/Grid_Glow_Discharge
+%ADB% %DEVICE% shell rm -r %STORAGE%/UE4Game/UE4CommandLine.txt
+%ADB% %DEVICE% shell rm -r %STORAGE%/obb/com.CryoVR.Grid_Glow_Discharge
+%ADB% %DEVICE% shell rm -r %STORAGE%/Android/obb/com.CryoVR.Grid_Glow_Discharge
+@echo.
+@echo Uninstall completed
